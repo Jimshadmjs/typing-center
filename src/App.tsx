93590338100c361abddda8543fdd3a95fded952e
Jimshad <jimshd925@gmail.com>
2025-12-10 12:23:27 +0530

    feat(router): add routing structure and private route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useAuth } from "./contexts/AuthContext";
+import LoginPage from "./pages/LoginPage";
+import GalleryPage from "./pages/GalleryPage";
+import type { JSX } from "react";
 
-const App = () => {
+const PrivateRoute = ({ children }: { children: JSX.Element }) => {
+  const { user, loading } = useAuth();
+
+  if (loading) return <p>Loading...</p>;
+
+  return user ? children : <Navigate to="/login" replace />;
+};
+
+export default function App() {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
 
-export default App
+        <Route
+          path="/gallery"
+          element={
+            <PrivateRoute>
+              <GalleryPage />
+            </PrivateRoute>
+          }
+        />
+
+        <Route path="*" element={<Navigate to="/gallery" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
